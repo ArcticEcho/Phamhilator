d29df392230622463e5ebb3172badfc9de7f9551
Sam <s.kamber8@gmail.com>
2014-10-09 20:42:58 +0100

    Updated filters.

--- Phamhilator/PostAnalysis.cs
+++ Phamhilator/PostAnalysis.cs
@@ -7,10 +7,6 @@ namespace Phamhilator
 {
 	public abstract class PostAnalysis
 	{
-		//public QuestionAnalysis QRsults = new QuestionAnalysis();
-
-		//public List<AnswerAnalysis> AResults = new List<AnswerAnalysis>();
-
 		public PostType Type;
 		public float Accuracy;
 		public readonly Dictionary<Regex, float> WhiteTermsFound = new Dictionary<Regex, float>();
