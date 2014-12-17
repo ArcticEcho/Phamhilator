5a719a6bfce89216ad77a80170265ad8a126ebc3
Sam <s.kamber8@gmail.com>
2014-12-17 09:01:23 +0000

    Improves rate limiting function.

--- Phamhilator/MessageHandler.cs
+++ Phamhilator/MessageHandler.cs
@@ -13,8 +13,8 @@ namespace Phamhilator
     public class MessageHandler : IDisposable
     {
         private bool disposed;
-        private Thread processor;
         private bool exit;
+        private readonly Thread processor;
         private readonly Dictionary<Room, List<DateTime>> lastPostRecord = new Dictionary<Room, List<DateTime>>();
 
         public delegate void MessagePostedCallBack();
@@ -45,6 +45,16 @@ namespace Phamhilator
             if (disposed) { return; }
 
             exit = true;
+            disposed = true;
+
+            // Give the thread a chance to exit gracefully.
+            Thread.Sleep(400); 
+
+            if (processor.IsAlive)
+            {
+                // Otherwise kill it with fire!
+                processor.Abort();
+            }
         }
 
         public void QueueItem(ChatAction message)
@@ -69,7 +79,7 @@ namespace Phamhilator
             {
                 while (Queue.Count == 0)
                 {
-                    Thread.Sleep(250);
+                    Thread.Sleep(200);
                 }
 
                 ChatAction nextM;
@@ -84,17 +94,10 @@ namespace Phamhilator
 
                 if (rateLimit > 0)
                 {
-                    Thread.Sleep(TimeSpan.FromSeconds(rateLimit + 1));
+                    Thread.Sleep(TimeSpan.FromSeconds(rateLimit /*+ 1*/));
                 }
 
                 nextM.Action();
-
-                //if (nextM.Message.IsReply)
-                //{
-                //    nextM.Callback(nextM.Room.PostReply(nextM.Message));
-                //}
-
-                //nextM.Callback(nextM.Room.PostMessage(nextM.Message));
 
                 lock (Queue)
                 lock (lastPostRecord)
@@ -113,56 +116,38 @@ namespace Phamhilator
             }
         }
 
-        private double CalcRateLimit(List<DateTime> messageRecords)
+        private static double CalcRateLimit(List<DateTime> messageRecords)
         {
             if (messageRecords == null || messageRecords.Count == 0) { return 0; }
 
             var limit = 0.0;
-            var a = messageRecords.Count - 1;
-            var b = 0.0;
-            var throttled = false;
+            var lastClearIndex = messageRecords.Count - 1;
+            var currentThrottleDur = 0.0;
 
-            for (var i = messageRecords.Count - 1; i > 0; i--)
+            for (var i = messageRecords.Count - 1; i > -1; i--)
             {
-                limit = Limit(a - i);
-                var currentDur = (messageRecords[i] - messageRecords[a]).TotalSeconds;
+                limit = Limit(lastClearIndex - i);
+                var timeSinceClear = (messageRecords[lastClearIndex] - messageRecords[i]).TotalSeconds;
 
-                if (currentDur < limit && !throttled)
+                if (timeSinceClear < limit)
                 {
-                    throttled = true;
-
-                    b = limit - currentDur;
+                    currentThrottleDur = limit;
 
                     continue;
                 }
 
-                if (b - currentDur < 0)
+                if (timeSinceClear > limit * 2 || currentThrottleDur - timeSinceClear < 0)
                 {
-                    throttled = false;
-
-                    a = i;
-                }
-
-                if (currentDur > limit && !throttled)
-                {
-                    throttled = false;
-                }
-
-                if (currentDur > limit * 2)
-                {
-                    a = i;
-
-                    throttled = false;
+                    lastClearIndex = i;
                 }
             }
 
-            limit = Limit(a);
+            limit = Limit(lastClearIndex);
 
-            /*var t = (messageRecords[a] - messageRecords[0]).TotalSeconds;*/
-            return /*t -*/ limit;
+            return Math.Max(limit, 0);
         }
 
-        private double Limit(double x)
+        private static double Limit(double x)
         {
             return Math.Min((4.1484 * Math.Log(x) + 1.02242), 20);
         }
